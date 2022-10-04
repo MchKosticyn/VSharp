82a9f48b5a7acfbc670154fe6f7bb76ea0789116
Dmitry Mordvinov <dvvsrd@gmail.com>
2022-10-04 11:36:13 +0300

    All Microsoft methods made trusted and concretely runnable

--- VSharp.InternalCalls/Loader.fs
+++ VSharp.InternalCalls/Loader.fs
@@ -1,5 +1,6 @@
 namespace VSharp
 
+open Microsoft.VisualBasic.CompilerServices
 open global.System
 open System.Reflection
 
@@ -57,54 +58,20 @@ module Loader =
     let public getRuntimeExceptionsImplementation (fullMethodName : string) =
         runtimeExceptionsConstructors.[fullMethodName]
 
-    let public ConcreteInvocations =
-        set [
-            // Types
-            "System.Type System.Type.GetTypeFromHandle(System.RuntimeTypeHandle)"
-            "System.Type System.Type.GetTypeFromHandle(System.RuntimeTypeHandle)"
-            "System.Reflection.RuntimeAssembly System.RuntimeTypeHandle.GetAssembly(System.RuntimeType)"
-            "System.Type System.RuntimeType.GetElementType(this)"
-            "System.Boolean System.Type.op_Inequality(System.Type, System.Type)"
-            "System.Boolean System.Type.op_Equality(System.Type, System.Type)"
-            "System.Boolean System.RuntimeTypeHandle.IsGenericTypeDefinition(System.RuntimeType)"
-            "System.Boolean System.RuntimeTypeHandle.IsInterface(System.RuntimeType)"
-            "System.Boolean System.RuntimeTypeHandle.IsGenericVariable(System.RuntimeType)"
-            "System.String System.RuntimeType.get_Name(this)"
-            "System.Boolean System.Type.get_IsValueType(this)"
-            "System.Array System.RuntimeType.GetEnumValues(this)"
-            "System.Type System.RuntimeType.GetEnumUnderlyingType(this)"
-            "System.Boolean System.RuntimeTypeHandle.CanCastTo(System.RuntimeType, System.RuntimeType)"
-            "System.Reflection.RuntimeModule System.RuntimeTypeHandle.GetModule(System.RuntimeType)"
-            "System.Void System.ModuleHandle.GetModuleType(System.Runtime.CompilerServices.QCallModule, System.Runtime.CompilerServices.ObjectHandleOnStack)"
-            "System.Reflection.PropertyInfo System.Type.GetProperty(this, System.String, System.Reflection.BindingFlags)"
-            "System.Type[] System.RuntimeType.GetInterfaces(this)"
-            "System.Boolean System.Type.get_IsByRef(this)"
-            "System.RuntimeType System.RuntimeTypeHandle.GetBaseType(System.RuntimeType)"
-            "System.TypeCode System.Type.GetTypeCode(System.Type)"
+    // Set of methods that can be run concretely
+    let public TrustedMethods = set [
+        "System.Int32 IntegrationTests.ExceptionsControlFlow.ConcreteThrow()"
+    ]
+    let public TrustedModules = seq [
+        "System.";
+        "Microsoft."
+    ]
+    let public BlackListedMethods = set [
+    ]
 
-            // EqualityComparer
-            "System.Object System.Collections.Generic.ComparerHelpers.CreateDefaultEqualityComparer(System.Type)"
-            "System.Object System.Collections.Generic.ComparerHelpers.CreateDefaultComparer(System.Type)"
-            "System.Collections.Generic.EqualityComparer`1[T] System.Collections.Generic.EqualityComparer`1[T].get_Default()"
-
-            // Thread
-            "System.Threading.Thread System.Threading.Thread.get_CurrentThread()"
-
-            // Interop
-            "System.Int32 Interop+Sys.LChflagsCanSetHiddenFlag()"
-            "System.Byte* Interop+Sys.GetCwd(System.Byte*, System.Int32)"
-            "System.Object System.Runtime.InteropServices.GCHandle.InternalCompareExchange(System.IntPtr, System.Object, System.Object)"
-            "System.Boolean System.Runtime.Intrinsics.X86.Sse2.get_IsSupported()"
-
-            // Diagnostics
-            "System.IntPtr System.Diagnostics.Tracing.EventPipeInternal.CreateProvider(System.String, Interop+Advapi32+EtwEnableCallback)"
-            "System.Void System.Diagnostics.StackTrace.GetStackFramesInternal(System.Diagnostics.StackFrameHelper, System.Int32, System.Boolean, System.Exception)"
-
-            // LINQ
-            "System.Void System.Linq.Expressions.Expression.Validate(System.Type, System.Boolean)"
-
-            "System.Int32 IntegrationTests.ExceptionsControlFlow.ConcreteThrow()"
-        ]
-
-    let isInvokeInternalCall (fullMethodName : string) =
-        ConcreteInvocations.Contains fullMethodName
+    let isTrustedMethod (methodModule : string) (fullMethodName : string) =
+        let trusted =
+            TrustedModules |> Seq.exists (fun tm -> methodModule.StartsWith(tm, StringComparison.Ordinal)) ||
+            TrustedMethods.Contains fullMethodName
+        let blacklisted = BlackListedMethods.Contains fullMethodName
+        trusted && not blacklisted
