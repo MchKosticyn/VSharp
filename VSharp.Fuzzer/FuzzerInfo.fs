f5b6243d079db0dc18181f35904d966223e98993
MchKosticyn <mishakosticyn@yandex.ru>
2022-12-16 14:03:20 +0300

    [wip] state generation from fuzzer result

--- VSharp.Fuzzer/FuzzerInfo.fs
+++ VSharp.Fuzzer/FuzzerInfo.fs
@@ -1,7 +1,5 @@
 module VSharp.Fuzzer.FuzzerInfo
 
-open System.Reflection
-
 open VSharp.Core
 open VSharp
 
@@ -14,15 +12,22 @@ let defaultFuzzerConfig = {
 }
 
 type IFuzzer =
-    abstract member Fuzz : state -> int -> state seq
+    abstract member Fuzz : int -> state seq
+    abstract member FuzzWithState : state -> int -> state seq
     abstract member Configure: FuzzerConfig -> unit
 
 let mutable private fuzzer: IFuzzer option = None
 let SetFuzzer (newFuzzer: IFuzzer) = fuzzer <- Some newFuzzer
 let ConfigureFuzzer (config: FuzzerConfig) = fuzzer |> Option.iter (fun f -> f.Configure config)
 
-let Fuzz (state: state) =
+let FuzzWithState (state: state) =
     let seed = System.DateTime.Now.Millisecond
     match fuzzer with
-    | Some f -> f.Fuzz state seed
+    | Some f -> f.FuzzWithState state seed
+    | None -> failwith "Fuzzer is not configured"
+
+let Fuzz () =
+    let seed = System.DateTime.Now.Millisecond
+    match fuzzer with
+    | Some f -> f.Fuzz seed
     | None -> failwith "Fuzzer is not configured"
